a6086c37bc9185c310c3b598e300a46d9f187460
Matevž Fabjančič <mf6422@student.uni-lj.si>
2018-08-31 17:05:40 +0200

    Version bump for release 1.3.3

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ PYJVMGUI_HOME = os.path.join(os.path.expanduser("~"), '.pyjvmgui')
 
 setup(
     name='pyjvmgui',
-    version='1.3.2',
+    version='1.3.3',
     url='https://github.com/MatevzFa/pyjvm',
     license='GPL-3.0',
     author='matevz',
