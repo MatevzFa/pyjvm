1d0e8f57e523d7aad01b6326f27a1d97a9f658db
Matevž Fabjančič <mf6422@student.uni-lj.si>
2018-09-10 00:02:49 +0200

    Installation script fix and version bump for release 1.3.6

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ import os
 from os import path
 from sys import path as sys_path
 
-from pyjvm_util.globals import PYJVMGUI_HOME
 from setuptools import find_packages, setup
 from setuptools.command.develop import develop
 from setuptools.command.install import install
@@ -12,6 +11,7 @@ this_directory = path.abspath(path.dirname(__file__))
 srcdir = path.join(this_directory, "src/")
 sys_path.insert(0, srcdir)
 
+from pyjvm_util.globals import PYJVMGUI_HOME
 
 def post_install():
     try:
@@ -36,7 +36,7 @@ class InstallWithRt(install):
 
 setup(
     name='pyjvmgui',
-    version='1.3.5',
+    version='1.3.6',
     url='https://github.com/MatevzFa/pyjvm',
     license='GPL-3.0',
     author='matevz',
